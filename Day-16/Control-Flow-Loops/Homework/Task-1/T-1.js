7bbd93e18e3ec68fb6573d636a16fa55d21931bd
Tadeoz24 <tadeoz.ghvaladze@gmail.com>
2021-09-06 01:43:23 +0400

    first commit

---
+++ Day-16/Control-Flow-Loops/Homework/Task-1/T-1.js
@@ -0,0 +1,7 @@
+const numbers = [2, 5, 9, 15, 0, 4];
+
+for (const number of numbers) {
+  if (number > 3 && number < 10) {
+    console.log(number);
+  }
+}
---
+++ Day-16/Control-Flow-Loops/Homework/Task-2/T-2.js
@@ -0,0 +1,14 @@
+var n = 1000;
+let result;
+var num = 0;
+
+while (n > 50) {
+    result = n / 2;
+    if (result < 50) {
+        break;
+    }
+    console.log(result);
+    n -= result;
+    num += 1;
+}
+console.log("sum of the while loop itteration = " + num);
