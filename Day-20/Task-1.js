2bc53e0d60dec9186198c371bc21281ea2dd1c24
Tadeozi <Tadeozi@TADEOZI-NB>
2021-09-20 19:30:44 +0400

    done

---
+++ Day-20/Task-1.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.prototype.mergeDeepRight = function (args) {
+  for (key in args) {
+    if (args[key] == mergeDeepRight) continue;
+    else if (typeof args[key] !== "object") this[key] = args[key];
+    else {
+      if (Array.isArray(args[key])) {
+        for (i of args[key]) {
+          this[key].unshift(i);
+        }
+      } else if (this.hasOwnProperty(key) == false) {
+        this[key] = {};
+      }
+      this[key].mergeDeepRight(args[key]);
+    }
+  }
+};
+
+const data = {
+  name: "fred",
+  age: 10,
+  contact: {
+    email: "moo@example.com",
+    meta: {
+      verified: true,
+      tags: ["important"],
+    },
+  },
+};
+data.mergeDeepRight({
+  age: 40,
+  contact: {
+    email: "baa@example.com",
+    favorite: true,
+    meta: {
+      tags: ["vip"],
+    },
+  },
+});
+console.log(data);
